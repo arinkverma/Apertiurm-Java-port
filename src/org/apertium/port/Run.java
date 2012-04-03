6e3ddefaa823f14351039459506f589d5fee5419
Arink Verma <arinkverma@gmail.com>
2012-04-04 02:24:50 +0530

    first commit

--- src/org/apertium/port/Run.java
+++ src/org/apertium/port/Run.java
@@ -4,6 +4,18 @@
  */
 package org.apertium.port;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+
+import org.apertium.pipeline.ApertiumMain;
+
 
 /**
  *
@@ -23,21 +35,47 @@ public class Run {
 	      System.err.println("ApertiumPort: A java port translator for English <-> Esperanto\n" +
 	          "USAGE: "+invocationCommand+"\n" +
 	          "Examples:\n" +
-	          "<Data Dir>    <\"translate text from English to Esperanto\">\n" +
+	          "<Path of input file>" +
 	          "<\"translate text from English to Esperanto\">\n");
 	      System.exit(-1);
 	  }
 	
-    public static void main(String[] args) throws Exception {
+	private static String readFile( String file ) throws IOException {
+	    BufferedReader reader = new BufferedReader( new FileReader (file));
+	    String line  = null;
+	    StringBuilder stringBuilder = new StringBuilder();
+	    String ls = System.getProperty("line.separator");
+	    while( ( line = reader.readLine() ) != null ) {
+	        stringBuilder.append( line );
+	        stringBuilder.append( ls );
+	    }
+	    return stringBuilder.toString();
+	 }
+	
+    public static void main(String[] args) {
     	String output = "";
+    	String f = "";
     	if (args.length == 0){ showHelp(null);
     	
     	}else if(args.length == 1){
-    		output = Port.get(args[0],"eo-en");
+    		f = args[0];
     	}else if(args.length ==2){
     		Port.setAddresss(args[0]);
-    		output = Port.get(args[1],"eo-en");
+    		f = args[1];   		
     	}
-        System.out.println(output);
+    	
+    	String input;
+		try {
+			input = readFile(f);
+			output = Port.get(input,"eo-en");
+	        System.out.println(output);
+		} catch (IOException e) {
+			System.out.println("Invalid input path");
+			showHelp(null);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}    		
+
     }
 }
